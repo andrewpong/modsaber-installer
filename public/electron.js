c43959f003df8751abef8a17c3721a49b2fe18f4
Jack Baron <jackmbaron@gmail.com>
2018-11-28 13:25:59 +0000

    CRA+Electron+Builder Skeleton

---
+++ public/electron.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const { app, BrowserWindow, dialog, Menu } = require('electron')
+const { autoUpdater } = require('electron-updater')
+const isDev = require('electron-is-dev')
+
+/**
+ * @type {BrowserWindow}
+ */
+let window
+
+app.on('ready', () => {
+  if (!isDev) autoUpdater.checkForUpdates()
+
+  const height = 500
+  window = new BrowserWindow({
+    width: 700,
+    height: isDev ? height + 20 : height,
+    show: false,
+  })
+
+  const menu = !isDev ? null : new Menu.buildFromTemplate([ // eslint-disable-line
+    {
+      label: 'Dev',
+      submenu: [
+        { role: 'reload' },
+        { role: 'forcereload' },
+        { role: 'toggledevtools' },
+      ],
+    },
+  ])
+  window.setMenu(menu)
+
+  const startURL = isDev ?
+    'http://localhost:3000' :
+    `file://${path.join(__dirname, '../build/index.html')}`
+  window.loadURL(startURL)
+
+  window.setTitle(`ModSaber Installer // v${require('../package.json').version}`)
+  window.show()
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+autoUpdater.on('download-progress', percent => {
+  window.setProgressBar(percent.percent, { mode: 'normal' })
+})
+
+autoUpdater.on('update-downloaded', () => {
+  dialog.showMessageBox({
+    type: 'info',
+    buttons: [],
+    title: 'Updater',
+    message: 'A newer version has been downloaded.\n\nClick OK to install the update.\nThe program will restart with the update applied.',
+  })
+
+  autoUpdater.quitAndInstall(true, true)
+})
--- src/js/App.jsx
+++ src/js/App.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import '../css/App.css'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <header className="App-header">
@@ -21,8 +21,8 @@ class App extends Component {
           </a>
         </header>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
