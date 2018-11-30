4627293177bfae66e84f460f2825fbea2c8b9a50
Jack Baron <jackmbaron@gmail.com>
2018-11-30 00:35:27 +0000

    More credits

--- src/js/components/tabs/Credits.jsx
+++ src/js/components/tabs/Credits.jsx
@@ -4,10 +4,17 @@ import ExtLink from '../ExtLink.jsx'
 const Credits = () =>
   <div className='content'>
     <h1>ModSaber and ModSaber Installer</h1>
-    <p>Written by <ExtLink href='https://github.com/lolPants'>lolPants</ExtLink></p>
+    <p>
+      Written by <ExtLink href='https://github.com/lolPants'>lolPants</ExtLink> with
+      code from <ExtLink href='https://github.com/lolPants/modsaber-installer/graphs/contributors'>Additional Contributors</ExtLink>
+    </p>
 
-    <h1>Additional Contributors</h1>
-    <p>Available on <ExtLink href='https://github.com/lolPants/modsaber-installer/graphs/contributors'>GitHub</ExtLink></p>
+    <h1>Inspiration</h1>
+    <p>
+      This was inspired by the
+      original <ExtLink href='https://github.com/Umbranoxio/BeatSaberModInstaller'>Mod
+      Manager</ExtLink> by <ExtLink href='https://github.com/Umbranoxio'>Umbranox</ExtLink>
+    </p>
   </div>
 
 export default Credits
