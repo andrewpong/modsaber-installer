45db5d7b54c60527b1aa127325b20ed694433fdd
Jack Baron <jackmbaron@gmail.com>
2018-12-06 14:57:06 +0000

    Refactor status fragment

--- src/js/components/main/Status.jsx
+++ src/js/components/main/Status.jsx
@@ -1,11 +1,11 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
 const Status = props =>
-  <Fragment>
+  <>
     <i className={ `${props.icon || 'fas fa-cog'} fa-2x${props.spin ? ' fa-spin' : ''}` }></i>
     <span style={{ marginTop: '5px', textAlign: 'center' }}>{ props.children }</span>
-  </Fragment>
+  </>
 
 Status.propTypes = {
   icon: PropTypes.string.isRequired,
