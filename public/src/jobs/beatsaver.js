b243b7ebf52a69182d54032c78c66f40e1af30cf
Jack Baron <jackmbaron@gmail.com>
2018-12-12 17:49:42 +0000

    Add status text to beatsaver job

--- public/src/jobs/beatsaver.js
+++ public/src/jobs/beatsaver.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const { BrowserWindow } = require('electron')
 const fse = require('../utils/file.js')
 const { findPath } = require('../logic/pathFinder.js')
 const { extractZip, safeDownload } = require('../remote/remote.js')
@@ -11,7 +12,15 @@ class BeatSaverError extends Error {
   }
 }
 
-const downloadSong = async input => {
+/**
+ * @param {string} input BeatSaver Input (hash or key)
+ * @param {BrowserWindow} win Browser Window
+ */
+const downloadSong = async (input, win) => {
+  // Window Details
+  const window = win || BrowserWindow.getAllWindows()[0]
+  const sender = window.webContents
+
   // Find install path
   const install = await findPath()
   if (install.platform === 'unknown') throw new BeatSaverError('Could not find your Beat Saber directory.\nRun the mod manager once first!')
@@ -24,13 +33,18 @@ const downloadSong = async input => {
   const type = inputType(input)
   if (type === 'invalid') throw new BeatSaverError('Invalid Song ID / Hash!')
 
-  // Download song zip
+  // Download song info
+  sender.send('set-status', { text: 'Downloading song info...' })
   const song = await (type === 'hash' ? fromHash(input) : fromID(input))
+
+  // Download song zip
+  sender.send('set-status', { text: 'Downloading song zip...' })
   const zip = await safeDownload(song.downloadUrl)
   if (zip.error) throw new BeatSaverError('Song Download Failed!')
 
   try {
     // Extract zip
+    sender.send('set-status', { text: 'Extracting song...' })
     const files = await extractZip(zip.body, path.join(customSongs, song.key))
 
     // File Write Jobs
@@ -43,6 +57,7 @@ const downloadSong = async input => {
 
     // Flush all jobs and return
     await Promise.all(jobs)
+    sender.send('set-status', { text: 'Song install complete!' })
     return undefined
   } catch (err) {
     throw new BeatSaverError('Extraction Failure!')
