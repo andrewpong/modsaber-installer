a1d7a540f092cb56f6a5b82e6b593c0a184ea24f
Jack Baron <jackmbaron@gmail.com>
2018-12-12 03:46:43 +0000

    Refactor remote logic to own file

--- public/src/logic/modsaber.js
+++ public/src/logic/modsaber.js
@@ -1,6 +1,5 @@
-const path = require('path')
-const AdmZip = require('adm-zip')
 const { get } = require('snekfetch')
+const { extractZip, safeDownload } = require('./remote.js')
 const { calculateHash } = require('./helpers.js')
 const { API_URL, USER_AGENT } = require('../constants.js')
 
@@ -44,43 +43,6 @@ const fetchMods = async options => {
 const fetchGameVersions = async () => {
   const { body } = await get(`${API_URL}/site/gameversions`).set('User-Agent', USER_AGENT)
   return body
-}
-
-/**
- * @param {string} url URL
- * @returns {Promise.<{ error: boolean, body: Buffer}>}
- */
-const safeDownload = async url => {
-  try {
-    const resp = await get(url).set('User-Agent', USER_AGENT)
-
-    if (resp.statusCode !== 200) throw new Error('Status not 200')
-    else return { error: false, body: resp.body }
-  } catch (err) {
-    return { error: true, body: null }
-  }
-}
-
-/**
- * @param {Buffer} blob Zip Blob
- * @param {string} installDir Install Directory
- * @returns {Promise.<{ path: string, data: Buffer }[]>}
- */
-const extractZip = async (blob, installDir) => {
-  const zip = new AdmZip(blob)
-
-  const entries = zip.getEntries().map(entry => new Promise(resolve => {
-    if (entry.isDirectory) resolve(null)
-
-    // Filter out files that try to break out of the install dir
-    const fullPath = path.join(installDir, entry.entryName)
-    if (!fullPath.startsWith(installDir)) return resolve(null)
-
-    entry.getDataAsync(data => resolve({ path: path.join(installDir, entry.entryName), data }))
-  }))
-
-  const data = await Promise.all(entries)
-  return data.filter(x => x !== null)
 }
 
 class InstallError extends Error {
