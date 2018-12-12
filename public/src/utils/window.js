421c3d36574e95d55294f6a8ce469c800401cccf
Jack Baron <jackmbaron@gmail.com>
2018-12-12 17:48:26 +0000

    Add JSDoc to Window utils

--- public/src/utils/window.js
+++ public/src/utils/window.js
@@ -1,5 +1,9 @@
 const { shell } = require('electron')
 
+/**
+ * @param {Electron.BrowserWindow} window Window
+ * @returns {void}
+ */
 const getAttention = window => {
   if (window.isFocused()) return undefined
 
