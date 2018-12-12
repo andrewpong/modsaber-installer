6ba87ecb5689b6142cdc6b5a3d5d412a61573dfd
Jack Baron <jackmbaron@gmail.com>
2018-12-12 17:51:34 +0000

    Refactor installer event to use job system

--- public/src/events/installer.js
+++ public/src/events/installer.js
@@ -1,41 +1,11 @@
-const { ipcMain, dialog, BrowserWindow } = require('electron')
-const log = require('electron-log')
-const { getAttention } = require('../utils/window.js')
-const { InstallerError, installMods } = require('../jobs/installer.js')
-const { enqueueJob, dequeueJob } = require('../logic/queue.js')
+const { ipcMain, BrowserWindow } = require('electron')
+const { installMods } = require('../jobs/installer.js')
+const { runJob } = require('../jobs/job.js')
 
 ipcMain.on('install-mods', async ({ sender }, data) => {
   // Get Browser Window
   const window = BrowserWindow.fromWebContents(sender)
 
-  // Start job
-  const jobID = await enqueueJob()
-  window.setProgressBar(1, { mode: 'indeterminate' })
-
-  try {
-    await installMods(data.mods, data.install, data.gameVersion, window)
-    window.setProgressBar(1, { mode: 'normal' })
-  } catch (err) {
-    if (err instanceof InstallerError) {
-      log.debug(err)
-      window.setProgressBar(1, { mode: 'error' })
-
-      if (err.flash) getAttention(window)
-      if (err.status) sender.send('set-status', { text: err.status })
-
-      dialog.showMessageBox(window, {
-        type: 'error',
-        title: err.title,
-        message: err.message,
-      })
-    }
-  }
-
-  // Reset progress bar
-  setTimeout(() => {
-    window.setProgressBar(0, { mode: 'none' })
-  }, 500)
-
-  // Dequeue job
-  await dequeueJob(jobID)
+  const job = installMods(data.mods, data.install, data.gameVersion, window)
+  await runJob(job, window)
 })
