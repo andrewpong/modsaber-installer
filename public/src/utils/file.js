7cb1181e7862be82d6626694620870d6fe93cb0d
Jack Baron <jackmbaron@gmail.com>
2018-12-17 05:37:42 +0000

    Add isFile to FSE

--- public/src/utils/file.js
+++ public/src/utils/file.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const mkdirp = require('mkdirp')
 const { promisify } = require('util')
 const access = promisify(fs.access)
+const stat = promisify(fs.stat)
 
 /**
  * @param {fs.PathLike} path A path to a file. If a URL is provided, it must use the file: protocol.
@@ -17,11 +18,22 @@ const exists = async path => {
   }
 }
 
+/**
+ * @param {fs.PathLike} path A path to a file. If a URL is provided, it must use the file: protocol.
+ * URL support is experimental. If a file descriptor is provided, the underlying file will not be closed automatically.
+ */
+const isFile = async path => {
+  const stats = await stat(path)
+  return stats.isFile()
+}
+
 module.exports = {
   readFile: promisify(fs.readFile),
   writeFile: promisify(fs.writeFile),
   access,
+  stat,
   exists,
+  isFile,
   ensureDir: promisify(mkdirp),
   readDir: promisify(fs.readdir),
 }
